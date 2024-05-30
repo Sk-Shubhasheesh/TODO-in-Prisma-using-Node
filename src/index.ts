9c12c1a574c5cee7be6b95dc8b1bb995777192d5
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-05-30 17:16:32 +0530

    Updating User Done

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,27 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function insertUser(username: string, password: string, firstName: string, lastName: string) {
-  try {
-    const result = await prisma.user.create({
-      data: {
-        email: username,
-        password,
-        firstName,
-        lastName,
-      },
-    });
-    console.log(result);
-  } catch (error) {
-    console.error('Error inserting user:', error);
-  }
+interface UpdateParams {
+    firstName: string;
+    lastName: string;
 }
 
-insertUser('admin2', '123456', 'Shubham', 'Kundu');
+async function updateUser(username: string, {
+    firstName,
+    lastName
+}: UpdateParams) {
+  const res = await prisma.user.update({
+    where: {email: username },
+    data: {
+      firstName,
+      lastName
+    }
+  });
+  console.log(res);
+}
+
+updateUser("admin1", {
+    firstName: "new name",
+    lastName: "Gupta"
+})
